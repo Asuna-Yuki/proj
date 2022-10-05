a6d999aec7890a8b6e106d0788c3f00f9a46fbdf
ginpachiSensei <satyamrocker1234@gmail.com>
2022-10-06 03:42:13 +0530

    added structure of dealcard

--- src/components/landing.js
+++ src/components/landing.js
@@ -6,9 +6,9 @@ import DefaultCard from "./DefaultCard";
 export const Landing = () => {
   return (
     <Container>
-      <Button variant='primary'>Dashboard</Button>
-      <Button variant='primary'>Login Now</Button>
-      <Button variant='primary'>Back to Website</Button>
+      <Button variant="primary">Dashboard</Button>
+      <Button variant="primary">Login Now</Button>
+      <Button variant="primary">Back to Website</Button>
       <DefaultCard />
       <div>hello</div>
     </Container>
