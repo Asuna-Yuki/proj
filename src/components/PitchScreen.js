45f4e26155873d3da3511c4da37da6d4c486cc56
ginpachiSensei <satyamrocker1234@gmail.com>
2022-10-06 17:32:08 +0530

    added pitch screen component

---
+++ src/components/PitchScreen.js
@@ -0,0 +1,152 @@
+import React from "react";
+
+const PitchScreen = () => {
+  return (
+    <div className="pitch-card">
+      <div className="pitch-card-header">
+        <h1>Deck</h1>
+        <button>Download pitch</button>
+      </div>
+      <div className="pitch-card-body">
+        <>
+          <h3>10 Seconds Pitch</h3>
+          <p>
+            Online search, compare & booking platform for self-drive cars,
+            charter flights, living space & more that rewards users
+          </p>
+        </>
+        <hr />
+        <>
+          <h3>Deck Info</h3>
+          <h1>LetzRent</h1>
+        </>
+        <hr />
+        <>
+          <h3>Highlights</h3>
+          <ul>
+            <li>Running profitable pilot in Bengaluru</li>
+            <li>
+              20k+ registered users, 4.2 app rating and 10k+ social media
+              following
+            </li>
+            <li>Company is projecting 100cr+ transaction volume in 5 years</li>
+            <li>
+              Founder with the marketing expertise of 17+ years in Reliance,
+              Naaptol, Tv9 and Zee and more.
+            </li>
+            <li>GenZ CTO and co-founder.</li>
+            <li>Expansion is easy and the untapped customer base is huge.</li>
+            <li>Been in news articles, already making buzz.</li>
+          </ul>
+        </>
+        <hr />
+        <>
+          <h3>Traction</h3>
+          <img src="https://images.unlayer.com/projects/15608/1651352968445-10.PNG" />
+        </>
+        <hr />
+        <>
+          <h3>Smarter Way To Rent</h3>
+          <img src="https://images.unlayer.com/projects/15608/1651352942115-6.png" />
+        </>
+        <hr />
+        <>
+          <h3>Customers Want Low Prices</h3>
+          <p>
+            To get low prices 85% of the consumers check at least 2 data points
+            (Source: BCG Consumer Trends). In various sectors information
+            centred buying has been made more efficient by companies like MMT
+            for Travel, Policy bazaar for Insurance, Myntra or Shopperstop for
+            Lifestyle products. However for rental products, there is no one &
+            to fill this need and make the process efficient we launched
+            LetzRent, hoping to have a first-mover advantage and dominate the
+            market in the coming future.
+          </p>
+          <img src="https://images.unlayer.com/projects/15608/1651352925215-3.PNG" />
+        </>
+        <hr />
+        <>
+          <h3>Single KYC</h3>
+          <p>
+            Renting is a KYC dependent process, every time a customer books a
+            service from a different service provider he/she will have to get a
+            new KYC done. With Letzrent, the hassle of multiple KYC gets removed
+            and with a single KYC, customers can rent from multiple brands.
+          </p>
+        </>
+        <hr />
+        <>
+          <h3>Vast Selection</h3>
+          <p>
+            Being an aggregator Letzrent offers more options than category
+            leaders.
+          </p>
+          <img src="https://images.unlayer.com/projects/15608/1651352918059-2.png" />
+        </>
+        <hr />
+        <>
+          <h3>Why Renting?</h3>
+          <ol type="1">
+            <li>
+              Demographic Twist: Commitment phobic millennials prefer an asset
+              lite life with a bias toward renting rather than buying.
+            </li>
+            <li>
+              Flexibility: Renting allows people to upgrade or change assets
+              easily.
+            </li>
+            <li>
+              Convenience: With the rental customers dispense with the headache
+              of down payment, maintenance, insurance etc.
+            </li>
+          </ol>
+          <img src="https://images.unlayer.com/projects/15608/1651352954073-8.png" />
+        </>
+        <hr />
+        <>
+          <h3>Market Size</h3>
+          <img src="https://images.unlayer.com/projects/15608/1651352961142-9.png" />
+        </>
+        <hr />
+        <>
+          <h3>Competitors</h3>
+          <img src="https://images.unlayer.com/projects/15608/1651352937596-5.PNG" />
+        </>
+        <hr />
+        <>
+          <h3>Feedback: People love us</h3>
+          <img src="https://images.unlayer.com/projects/15608/1651352929580-4.PNG" />
+        </>
+        <hr />
+        <>
+          <h3>Future and growth</h3>
+          <img src="https://images.unlayer.com/projects/15608/1651352949305-7.png" />
+        </>
+        <hr />
+        <>
+          <h3>Core Team</h3>
+          <img src="https://images.unlayer.com/projects/15608/1651352986615-11.png" />
+        </>
+        <hr />
+        <>
+          <h3>Documents for Investors</h3>
+          <ul>
+            <li>
+              <a href="https://www.google.com/search?client=firefox-b-d&q=Due+Diligence+Report">
+                Due Diligence Report
+              </a>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1i02nUH-27EhiROtNlvXOfkPP7iTfwjpw/view">
+                Certificate of Incorporation
+              </a>
+            </li>
+          </ul>
+        </>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default PitchScreen;
