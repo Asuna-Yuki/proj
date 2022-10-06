190493fdab9d568cdcb3fdc48e250de51a0e79e5
ginpachiSensei <satyamrocker1234@gmail.com>
2022-10-06 23:25:28 +0530

    icon color change

--- src/components/FounderCard.js
+++ src/components/FounderCard.js
@@ -2,43 +2,43 @@ import React from "react";
 
 const FounderCard = (props) => {
   return (
-    <div className='founder-card'>
-      <div className='founder-card-img'>
-        <img src={props.image} alt='founder-image' />
+    <div className="founder-card">
+      <div className="founder-card-img">
+        <img src={props.image} alt="founder-image" />
       </div>
       <h3>{props.name}</h3>
       <h4>CoFounder</h4>
-      <div className='founder-card-social'>
+      <div className="founder-card-social">
         {props.twitter === "None" ? (
-          <div className='btn-social btn-twitter'>
-            <i className='fab fa-twitter' />
+          <div className="btn-social">
+            <i className="fab fa-twitter" />
           </div>
         ) : (
-          <div className='btn-social btn-twitter'>
+          <div className="btn-social btn-twitter">
             <a href={props.twitter}>
-              <i className='fab fa-twitter'></i>
+              <i className="fab fa-twitter"></i>
             </a>
           </div>
         )}
         {props.facebook === "None" ? (
-          <div className='btn-social btn-facebook'>
-            <i className='fab fa-facebook' />
+          <div className="btn-social">
+            <i className="fab fa-facebook" />
           </div>
         ) : (
-          <div className='btn-social btn-facebook'>
+          <div className="btn-social btn-facebook">
             <a href={props.facebook}>
-              <i className='fab fa-facebook' />
+              <i className="fab fa-facebook" />
             </a>
           </div>
         )}
         {props.linkedin === "None" ? (
-          <div className='btn-social btn-chrome'>
-            <i className='fab fa-chrome' />
+          <div className="btn-social">
+            <i className="fab fa-chrome" />
           </div>
         ) : (
-          <div className='btn-social btn-chrome'>
+          <div className="btn-social btn-chrome">
             <a href={props.linkedin}>
-              <i className='fab fa-chrome' />
+              <i className="fab fa-chrome" />
             </a>
           </div>
         )}
