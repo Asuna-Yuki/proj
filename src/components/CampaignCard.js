ccd2f0f8f846100d76f9c5dea4723216922b9c54
ginpachiSensei <satyamrocker1234@gmail.com>
2022-10-06 05:34:42 +0530

    added campaign card component

---
+++ src/components/CampaignCard.js
@@ -0,0 +1,27 @@
+const CampaignCard = () => {
+  return (
+    <div className="deal-card">
+      <div className="deal-card-first">
+        <h1>0</h1>
+        <p>hello</p>
+      </div>
+      <div className="deal-card-second">
+        <h2>0</h2>
+        <p>hello</p>
+      </div>
+      <div className="deal-card-third">
+        <h2>0</h2>
+        <p>hello</p>
+      </div>
+      <div className="deal-card-fourth">
+        <button>
+          <i className="ni ni-money-coins" />
+          CampaignCard
+        </button>
+        <p>hello</p>
+      </div>
+    </div>
+  );
+};
+
+export default CampaignCard;
